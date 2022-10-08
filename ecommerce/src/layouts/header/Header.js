0516188e87151673d4503d132f032d99c9c67443
Dzmitry Chmel <chmel.dmitriy@mail.ru>
2022-10-08 20:51:50 +0300

    added react

---
+++ ecommerce/src/layouts/header/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+	return (
+		<header>
+			<div class="nav container">
+				<a href="#" class="logo">
+					Shop
+				</a>
+				<div class="nav-icons">
+					<i class="bx bx-log-in-circle"></i>
+					<i class="bx bxs-user-circle"></i>
+					<i class="bx bx-basket"></i>
+					<i class="bx bx-log-out-circle"></i>
+				</div>
+			</div>
+		</header>
+	);
+};
+
+export default Header;
