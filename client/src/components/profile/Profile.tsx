79d45ec33bfe96f7121764ec7a5e03c9bd576cef
Chmelya <chmel.dmitriy@mail.ru>
2022-10-18 23:07:42 +0300

    profile init

--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -2,8 +2,18 @@ import "./Profile.scss";
 
 const ProfileSection = () => {
 	return (
-		<section className="container">
+		<section className="profile-section container">
 			<h2 className="section-title">Profile</h2>
+			<div className="profile-content">
+				<div className="profile-card">
+					<div className="circular">
+						<img
+							className="profile-img"
+							src="https://img-9gag-fun.9cache.com/photo/a3Q5VW5_460s.jpg"
+						></img>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
